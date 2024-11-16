035f152b9addb2857685284f4353bf90b1eb8fac
Aryan Singhal <aryan.singhal@SB-129.local>
2024-11-16 19:32:56 +0530

    feat: add boilerplate with tailwind and rsbuild

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="bg-green-500 text-white p-10">
+      <h1>Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+    </div>
+  );
+};
+
+export default App;
